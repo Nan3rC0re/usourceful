e1f572688b5eb9ca8d759d739f8e79a3af805caf
Nana Kofi Okae <89530363+Nan3rOkae@users.noreply.github.com>
2024-11-13 18:44:52 -0600

    fixed eslint issue

--- app/career-paths/page.tsx
+++ app/career-paths/page.tsx
@@ -1,52 +1,52 @@
 import React from "react";
-import Link from "next/link";
+// import Link from "next/link";
 import EmptyResourceState from "@/components/my-ui/empty-source-state";
 
-interface Careers {
-  name: string;
-  link: string;
-  description: string;
-  img: string;
-}
+// interface Careers {
+//   name: string;
+//   link: string;
+//   description: string;
+//   img: string;
+// }
 
-const careers: Careers[] = [
-  {
-    name: "Frontend Engineer",
-    link: "/frontend-eng.mdx",
-    description: "A description for frontend engineering",
-    img: "",
-  },
-  {
-    name: "Backend Engineer",
-    link: "",
-    description: "A description for backend engineering",
-    img: "",
-  },
-  {
-    name: "Full Stack Engineer",
-    link: "",
-    description: "A description for full stack engineering",
-    img: "",
-  },
-  { name: "DevOps Engineer", link: "", description: "", img: "" },
-];
+// const careers: Careers[] = [
+//   {
+//     name: "Frontend Engineer",
+//     link: "/frontend-eng.mdx",
+//     description: "A description for frontend engineering",
+//     img: "",
+//   },
+//   {
+//     name: "Backend Engineer",
+//     link: "",
+//     description: "A description for backend engineering",
+//     img: "",
+//   },
+//   {
+//     name: "Full Stack Engineer",
+//     link: "",
+//     description: "A description for full stack engineering",
+//     img: "",
+//   },
+//   { name: "DevOps Engineer", link: "", description: "", img: "" },
+// ];
 
-const CareerItem = ({ name, link, description }: Careers) => {
-  return (
-    <Link
-      href={link}
-      target="_blank"
-      className="dark:hover:bg-white/5 hover:bg-neutral-100 rounded-2xl p-6  w-full text-start flex "
-    >
-      <div className="">
-        <h1 className="font-semibold max-sm:text-sm mb-1">{name}</h1>
-        <p className="text-sm w-full text-muted-foreground line-clamp-2 max-sm:text-xs">
-          {description}
-        </p>
-      </div>
-    </Link>
-  );
-};
+// const CareerItem = ({ name, link, description }: Careers) => {
+//   return (
+//     <Link
+//       href={link}
+//       target="_blank"
+//       className="dark:hover:bg-white/5 hover:bg-neutral-100 rounded-2xl p-6  w-full text-start flex "
+//     >
+//       <div className="">
+//         <h1 className="font-semibold max-sm:text-sm mb-1">{name}</h1>
+//         <p className="text-sm w-full text-muted-foreground line-clamp-2 max-sm:text-xs">
+//           {description}
+//         </p>
+//       </div>
+//     </Link>
+//   );
+// };
 
 export default function EarlyCareers() {
   return (
