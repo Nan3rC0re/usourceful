e52ecdc4863a02a62d87dae5d934761d64061e8e
Nana Kofi Okae <89530363+Nan3rOkae@users.noreply.github.com>
2025-02-28 18:10:53 -0500

    eslint issue fixed

--- components/Landing/CTA.tsx
+++ components/Landing/CTA.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import WaitlistEmail from "../emails/WaitlistEmail";
 
 const CTA = () => {
   return (
